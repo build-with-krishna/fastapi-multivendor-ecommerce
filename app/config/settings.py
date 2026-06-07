988006fcbe5a41beb98c21f91620ef2bd3c228da
Krishna Mahor <krishna.wcs2021@gmail.com>
2026-06-08 02:26:22 +0530

    migrate mysql to postgres

--- app/config/settings.py
+++ app/config/settings.py
@@ -1,10 +1,23 @@
 from pydantic_settings import BaseSettings
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 class Settings(BaseSettings):
-    
+
     DATABASE_URL: str
 
+    SECRET_KEY: str
+    ALGORITHM: str
+
+    DB_USER: str | None = None
+    DB_PASSWORD: str | None = None
+    DB_HOST: str | None = None
+    DB_PORT: int | None = None
+    DB_NAME: str | None = None
+
     class Config:
-        env_file = ".env"
+        env_file = BASE_DIR / ".env"
+        extra = "ignore"
 
 settings = Settings()
